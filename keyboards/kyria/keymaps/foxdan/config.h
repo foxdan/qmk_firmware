5e660aaa4993f04415aeeca5fb3d0f2a6bc01635
Dan <5571151+foxdan@users.noreply.github.com>
2020-08-03 00:41:04 +0100

    Add personal Kyria config.

---
+++ keyboards/kyria/keymaps/foxdan/config.h
@@ -0,0 +1,38 @@
+/* Copyright 2019 Thomas Baart <thomas@splitkb.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#ifdef OLED_DRIVER_ENABLE
+  #define OLED_DISPLAY_128X64
+#endif
+
+#ifdef RGBLIGHT_ENABLE
+  #define RGBLIGHT_ANIMATIONS
+  #define RGBLIGHT_HUE_STEP 8
+  #define RGBLIGHT_SAT_STEP 8
+  #define RGBLIGHT_VAL_STEP 8
+  #define RGBLIGHT_LED_MAP { 0, 1, 2, 9, 8, 7, 4, 3, 5, 6, 19, 18, 17, 10, 11, 12, 15, 16, 14, 13}
+  #define RGBLIGHT_EFFECT_KNIGHT_LED_NUM 12
+  #define RGBLIGHT_EFFECT_KNIGHT_OFFSET 14
+  #define RGBLIGHT_EFFECT_KNIGHT_LENGTH 3
+#endif
+
+#define EE_HANDS
+#define QMK_ESC_INPUT B4
+#define QMK_ESC_OUTPUT F4
+#define QMK_LED D5
+#define QMK_SPEAKER E0
